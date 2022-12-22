e3216c87ff7106643439f41baaa3405ebf257fc5
Shaik Hameed <skhameed@outlook.com>
2022-12-22 15:11:06 +0530

    Changed to lineage

--- lineage_m23xq.mk
+++ lineage_m23xq.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common.mk)
 
 # Inherit from m23xq device
