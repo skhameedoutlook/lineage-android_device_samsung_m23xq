4a393467b910d8fe4f7706865d5744c886d5fa7f
Shaik Hameed <skhameed@outlook.com>
2022-12-22 15:13:23 +0530

    some changes

--- device.mk
+++ device.mk
@@ -1,10 +1,9 @@
 #
-# Copyright (C) 2018-2022 The LineageOS Project
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Get non-open-source specific aspects
-$(call inherit-product, vendor/samsung/m23xq/m23xq-vendor.mk)
-
+LOCAL_PATH := device/samsung/m23xq
 
